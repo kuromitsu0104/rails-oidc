7e5143740836f5a5d3f3c0e682fb7dcdac31a3ae
kuromitsu0104 <kuromitsu0104@gmail.com>
2021-08-22 00:18:40 +0900

    [Edit] doorkeeper_openid_connectの設定変更

--- rails_open_id_provider/config/initializers/doorkeeper_openid_connect.rb
+++ rails_open_id_provider/config/initializers/doorkeeper_openid_connect.rb
@@ -2,20 +2,15 @@
 
 Doorkeeper::OpenidConnect.configure do
   issuer do |resource_owner, application|
-    'issuer string'
+    issuer { 'my_op' }
   end
 
-  signing_key <<~KEY
-    -----BEGIN RSA PRIVATE KEY-----
-    ....
-    -----END RSA PRIVATE KEY-----
-  KEY
+  signing_key File.read(Rails.root.join('jwtRS256.key'))
 
   subject_types_supported [:public]
 
   resource_owner_from_access_token do |access_token|
-    # Example implementation:
-    # User.find_by(id: access_token.resource_owner_id)
+    User.find_by(id: access_token.resource_owner_id)
   end
 
   auth_time_from_resource_owner do |resource_owner|
@@ -25,9 +20,9 @@ Doorkeeper::OpenidConnect.configure do
 
   reauthenticate_resource_owner do |resource_owner, return_to|
     # Example implementation:
-    # store_location_for resource_owner, return_to
-    # sign_out resource_owner
-    # redirect_to new_user_session_url
+    store_location_for resource_owner, return_to
+    sign_out resource_owner
+    redirect_to new_user_session_url
   end
 
   # Depending on your configuration, a DoubleRenderError could be raised
@@ -44,7 +39,7 @@ Doorkeeper::OpenidConnect.configure do
 
   subject do |resource_owner, application|
     # Example implementation:
-    # resource_owner.id
+    resource_owner.id
 
     # or if you need pairwise subject identifier, implement like below:
     # Digest::SHA256.hexdigest("#{resource_owner.id}#{URI.parse(application.redirect_uri).host}#{'your_secret_salt'}")
@@ -60,13 +55,13 @@ Doorkeeper::OpenidConnect.configure do
   # expiration 600
 
   # Example claims:
-  # claims do
+  claims do
   #   normal_claim :_foo_ do |resource_owner|
   #     resource_owner.foo
   #   end
 
-  #   normal_claim :_bar_ do |resource_owner|
-  #     resource_owner.bar
-  #   end
-  # end
+    normal_claim :email, scope: :openid do |resource_owner|
+      resource_owner.email
+    end
+  end
 end
